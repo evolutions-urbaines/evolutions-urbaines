7a84d9238b28c477f9286e8607e02ff79c9c0672
Francois Catuhe <fcatuhe@gmail.com>
2018-08-17 18:39:24 +0200

    simplify Navigation component and style Bootstrap

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,20 +2,11 @@ import React, { Component } from 'react'
 import Link from 'gatsby-link'
 import classNames from 'classnames'
 import onClickOutside from 'react-onclickoutside'
-import { Collapse, Navbar, NavbarBrand, Nav, NavItem } from 'reactstrap'
+import { Collapse } from 'reactstrap'
 
 class Navigation extends Component {
   state = {
     isOpen: false,
-    isTop: true,
-  }
-
-  componentDidMount() {
-    document.addEventListener('scroll', this.handleScroll)
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('scroll', this.handleScroll)
   }
 
   toggleNavbar = () => {
@@ -30,30 +21,24 @@ class Navigation extends Component {
 
   handleClickOutside = this.closeNavbar
 
-  handleScroll = () => {
-    const isTop = window.scrollY < 30
-    if (isTop !== this.state.isTop) {
-      this.setState({ isTop })
-    }
-  }
-
   classes = () =>
-    classNames('navigation', 'fixed-top', {
-      'navigation-simple': ['/coolworking', '/coolworking/'].includes(
-        this.props.pathname
-      ),
-      'navigation-top': this.state.isTop,
-      'navigation-opened': this.state.isOpen,
-    })
+    classNames(
+      'navigation',
+      'sticky-top',
+      {
+        'navigation-opened': this.state.isOpen,
+      },
+      'text-primary'
+    )
 
   render() {
     return (
       <div className={this.classes()}>
-        <Navbar dark expand="md">
+        <nav className="navbar navbar-expand-md navbar-light bg-white">
           <div className="container">
-            <NavbarBrand href="/" onClick={this.closeNavbar}>
-              Evolutions<br />Urbaines
-            </NavbarBrand>
+            <Link to="/" className="navbar-brand">
+              <h4 className="mb-0">Evolutions Urbaines</h4>
+            </Link>
             <button className="navbar-toggler" onClick={this.toggleNavbar}>
               <span className="navbar-toggler-icon" />
             </button>
@@ -62,16 +47,20 @@ class Navigation extends Component {
               onClick={this.closeNavbar}
               navbar
             >
-              <Nav navbar>
-                <NavItem className="px-3">
-                  <Link to="/" className="nav-link" activeClassName="active">
-                    Accueil
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link
+                    to="/autopartage"
+                    className="nav-link"
+                    activeClassName="active"
+                  >
+                    Autopartage
                   </Link>
-                </NavItem>
-              </Nav>
+                </li>
+              </ul>
             </Collapse>
           </div>
-        </Navbar>
+        </nav>
       </div>
     )
   }
