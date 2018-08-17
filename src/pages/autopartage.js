e2396c54c620884b5095a98d5fd34709ef39860e
Francois Catuhe <fcatuhe@gmail.com>
2018-08-17 18:39:34 +0200

    add autopartage page

---
+++ src/pages/autopartage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const CarSharingPage = ({ location }) => (
+  <Layout {...{ location }}>
+    <h1>Notre vision de l'autopartage</h1>
+
+    <h2>Véhicules neufs</h2>
+    <p>Plus de renseignements sur notre article.</p>
+
+    <h2>Service tout inclus</h2>
+    <p>Service ou particulier ?</p>
+  </Layout>
+)
+
+export default CarSharingPage
