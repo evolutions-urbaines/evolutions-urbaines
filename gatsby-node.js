14f99b11521937c3d02493ce1402f5d642bedb42
Francois Catuhe <fcatuhe@gmail.com>
2018-08-23 18:13:06 +0200

    clean gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -73,7 +73,6 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
             },
           })
           .then(({ data }) => {
-            console.log(data.title)
             createNodeField({
               node,
               name: 'url',
